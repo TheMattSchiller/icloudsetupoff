596ba278445e9cffa99eee9660aa59fa53ec19cc
Matt Schiller <itsmattjs@gmail.com>
2017-01-26 13:12:13 -0800

    added echos for monitoring and optimized, ready for production

--- icloudsetupoff.sh
+++ icloudsetupoff.sh
@@ -3,53 +3,68 @@
 # Determine OS version
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 sw_vers=$(sw_vers -productVersion)
-users_folder="/Users/"
-templates_folder="/System/Library/User Template/"
 
- # Checks the existing user folders in /Users
- # for the presence of the Library/Preferences directory.
- # If the directory is not found, it is created and then the
- # plist is created if it does not exist.
- # Then iCloud pop-up settings are set to be disabled.
+#users folder location, template folders location
+#and plist location within users folder
+users_folder="/Users"
+templates_folder="/System/Library/User Template"
+plist="/Library/Preferences/com.apple.SetupAssistant"
+
+#Checks the existing user folders in /Users
+#for the presence of the Library/Preferences directory.
+#If the directory is not found, it is created and then the
+#plist is created if it does not exist.
+#Then iCloud pop-up settings are set to be disabled.
 icloud_setup_off ()
 {
-for user in "$1"
+cd "$1"
+for user in *
   do
-    user_uid=`basename "${user}"`
+    echo
+    echo "Disabling iCloud setup and Setup Assistant for $user ..."
+    if [ "$1" = "$templates_folder" ]
+    then
+      user_uid="root"
+    else
+      user_uid=`basename "${user}"`
+    fi
     if [ ! "${user_uid}" = "Shared" ]
     then
       if [ ! -d "${user}"/Library/Preferences ]
       then
+        echo "creating preferences directory"
         mkdir -p "${user}"/Library/Preferences
         chown "${user_uid}" "${user}"/Library
         chown "${user_uid}" "${user}"/Library/Preferences
       fi
-      if [ ! -a "${user}"/Library/Preferences/com.apple.SetupAssistant ]
+      if [ ! -a "${user}""$plist" ]
       then
-        cp /tmp/com.apple.SetupAssistant.plist "${user}"/Library/Preferences/com.apple.SetupAssistant.plist
+        echo "creating empty plist for $user"
+        touch "${user}""$plist".plist
       fi
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeCloudSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeApplePaySetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeAvatarSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeSiriSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeSyncSetup2 -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeTouchIDSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant DidSeeiCloudLoginForStorageServicesSetup -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant SkipFirstLoginOptimization -bool TRUE
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant GestureMovieSeen none
-      defaults write "$user"/Library/Preferences/com.apple.SetupAssistant LastSeenCloudProductVersion "${sw_vers}"
-      chown "${user_uid}" "${user}"/Library/Preferences/com.apple.SetupAssistant.plist
+      echo "writing plist for user..."
+      echo "$1"/"$user""$plist"
+      defaults write "$1"/"$user""$plist" DidSeeCloudSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeApplePaySetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeAvatarSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeSiriSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeSyncSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeSyncSetup2 -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeTouchIDSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" DidSeeiCloudLoginForStorageServicesSetup -bool TRUE
+      defaults write "$1"/"$user""$plist" SkipFirstLoginOptimization -bool TRUE
+      defaults write "$1"/"$user""$plist" GestureMovieSeen none
+      defaults write "$1"/"$user""$plist" LastSeenCloudProductVersion "${sw_vers}"
+      defaults write "$1"/"$user""$plist" RegisteredVersion "${sw_vers}"
+      chown "${user_uid}" "${user}""$plist".plist
     fi
 done
 }
 
 #run on users folder
-icloud_setup_off $users_folder
+icloud_setup_off "$users_folder"
 
 #run on templates folder
-icloud_setup_off $templates_folder
-
-rm /tmp/com.apple.SetupAssistant.plist
+icloud_setup_off "$templates_folder"
 
 exit
